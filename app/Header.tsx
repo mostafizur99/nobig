8cbbf2921cbba45bba74d2267eb0ab78f2b5a178
mostafizur99 <coder.mr11@gmail.com>
2022-11-16 21:22:56 +0600

    feat: new page layout added

---
+++ app/Header.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header className='px-2 py-5 bg-slate-400'>Header</header>
+    )
+}
+
+export default Header
---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from './Header'
+
+const Home = () => {
+    return (
+        <>
+            <Header />
+            <div className='text-red-500'>Home page</div>
+        </>
+    )
+}
+
+export default Home
